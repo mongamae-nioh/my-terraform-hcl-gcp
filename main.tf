808924965bfe34a92fa0ed9ec7c0c1d092e2114c
mongamae-nioh <jkawaoka@gmail.com>
2021-02-16 22:37:55 +0900

    update main.cf and add variables.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ terraform {
 
 provider "google" {
 
-  credentials = file("creadential.json")
+  credentials = file(var.credentials_file)
 
-  project = "pure-spirit-210213"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "pure-spirit-210213"
+ }
+
+variable "credentials_file" {
+    default = "credential.json"
+ }
+
+variable "region" {
+  default = "us-central1"
+}
+
+variable "zone" {
+  default = "us-central1-c"
+}
